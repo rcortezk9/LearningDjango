b29b1a8a4ddf3c1c4912806eb4825236c151d7e8
Rene Cortez <rcortezk9@gmail.com>
2019-02-11 21:54:26 -0600

    Refractor directory name

--- LearningDjango/settings.py
+++ LearningDjango/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Test_App.apps.TestAppConfig',
     'rest_framework',
 ]
 
