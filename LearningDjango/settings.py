36e18139bdd42c626a8dbd054028b8c0225f27e9
Rene Cortez <rcortezk9@gmail.com>
2019-06-13 20:48:32 -0500

    Updated setting

--- LearningDjango/settings.py
+++ LearningDjango/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7eethvl(du$&q%if)icmgi=&meu0$hos!rz&r9)^2uzj9w1s9b'
+# SECRET_KEY = '7eethvl(du$&q%if)icmgi=&meu0$hos!rz&r9)^2uzj9w1s9b'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['demo-learningdjango.herokuapp.com']
 
 
 # Application definition
