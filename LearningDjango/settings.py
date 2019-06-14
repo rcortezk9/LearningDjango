82d9fccd95e438c4af2facb119ccc7be75615721
Rene Cortez <rcortezk9@gmail.com>
2019-06-13 21:21:25 -0500

    Remove the django-heroku

--- LearningDjango/settings.py
+++ LearningDjango/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,5 +123,3 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-django_heroku.settings(locals())
